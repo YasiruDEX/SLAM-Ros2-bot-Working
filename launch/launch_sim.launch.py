bef7c86aa266ad2890a30bf5e2692613eeb76999
Basnayake B.Y.N <131932017+YasiruDEX@users.noreply.github.com>
2024-06-14 15:28:19 +0530

    update: ROS 2 control plugin and gazebo control switch

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -22,13 +22,17 @@ def generate_launch_description():
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
+                
     )
+
+    gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
              )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
@@ -43,10 +47,10 @@ def generate_launch_description():
         arguments=["diff_cont"],
     )
 
-    joint_board_spawner = Node(
+    joint_broad_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["joint_board"],
+        arguments=["joint_broad"],
     )
 
     # Launch them all!
@@ -55,5 +59,5 @@ def generate_launch_description():
         gazebo,
         spawn_entity,
         diff_drive_spawner,
-        joint_board_spawner
+        joint_broad_spawner
     ])
